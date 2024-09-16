b1d29ac6631be9448863e855e2e21d1a397524bb
yurycash <157968112+yurycash@users.noreply.github.com>
2024-09-16 15:01:31 +0300

    Update test_api_1.py

--- test_api.py
+++ test_api.py
@@ -1,10 +1,12 @@
 import requests
 import allure
+from faker import Faker
 
 
 chitaigorod = "https://web-gate.chitai-gorod.ru/api/v1/"
-token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwczovL3VzZXItcmlnaHQiLCJzdWIiOjIwOTk1MTY2LCJpYXQiOjE3MjYyMjMyODcsImV4cCI6MTcyNjIyNjg4NywidHlwZSI6MjB9.NF3NTYZrKw0xTf7AKnoeHPTmss6Bal2k8UKAYasVwvI"
+token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwczovL3VzZXItcmlnaHQiLCJzdWIiOjIwOTk1MTY2LCJpYXQiOjE3MjY0ODU1ODQsImV4cCI6MTcyNjQ4OTE4NCwidHlwZSI6MjB9.xk-jut_rCeiJzeFGLCW4BzTXEaiHyVSf_qfC5h37Fdc"
 headers = {"authorization": f"Bearer {token}"}
+fake = Faker()
 
 
 @allure.feature("API")
@@ -24,6 +26,11 @@ def test_add_cart_product():
 @allure.title("Изменение кол-ва товара в корзине.")
 @allure.description("Изменение кол-ва товара в корзине через API.")
 def test_changing_quantity_in_cart():
+    body = {"id": 3038460, "adData": {
+        "item_list_name": "profile-bookmarks", "product_shelf": ""}}
+    with allure.step("Добавление товара в корзину."):
+        response = requests.post(
+            chitaigorod + "cart/product", headers=headers, json=body)
     body = [{"id": 125362739, "quantity": 10}]
     with allure.step("Изменение количества товара."):
         response = requests.put(chitaigorod + "cart",
@@ -64,7 +71,6 @@ def test_editing_personal_data():
         body = {"lastName": "Иванов",
                 "firstName": "Иван",
                 "middleName": "Иванович",
-                "birthday": "1980-01-01",
                 "phone": "79999999999",
                 "email": "yury.cash@yandex.ru",
                 "phoneCountry": ""}
